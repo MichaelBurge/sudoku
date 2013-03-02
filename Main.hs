4fad0d9a46c8269242b25321bf2ebc7d6f55978d
Michael Burge <michael.burge@rentrak.com>
2013-03-02 01:35:04 -0800

    Rewritten to store possibility lists rather than Maybes on each cell; basic applying of filters works(try all possibilities and keep ones that don't contradict)

--- Main.hs
+++ Main.hs
@@ -5,11 +5,11 @@ import Data.List
 import Data.Function
 import Data.Maybe
 
-type Row = [Maybe Integer]
+type Row = [[Integer]]
 type Board = [Row]
 
-sample ∷ Board
---sample = take 9 $ repeat [1..9]
+--sample ∷ Board
+--sample = take 9 $ repeat [1..9
 sample = [
  [ 1, 2, 3, 4, 5, 6, 7, 8, 9],
  [ 1, 2, 3, 4, 5, 6, 7, 8, 9],
@@ -21,10 +21,10 @@ sample = [
  [ 1, 2, 3, 4, 5, 6, 7, 8, 9],
  [ 1, 2, 3, 4, 5, 6, 7, 8, 9]]
 
-sample2 ∷ Board
+--sample2 ∷ Board
 sample2 = transpose sample
 
-sample3 ∷ Board
+--sample3 ∷ Board
 sample3 = [
  [ 2,4,8,3,9,5,7,1,6],
  [ 5,7,1,6,2,8,3,4,9],
@@ -36,21 +36,25 @@ sample3 = [
  [ 1,9,5,2,8,6,4,3,7],
  [ 4,2,7,9,5,3,8,6,1]]
 
+type Filter = Board → Bool
+
 rowUnique ∷ Row → Bool
-rowUnique ys = let zs = filter isJust ys in ((==) `on` length) zs (nub zs)
+rowUnique ys = 
+    let zs = filter (\xs → (length xs) == 1) ys
+    in ((==) `on` length) zs (nub zs)
+
 numElemsGood ∷ Row → Bool
 numElemsGood ys = (length ys) == 9
 rowGood ∷ Row → Bool
---rowGood ys = rowUnique ys ∧ numElemsGood ys
 rowGood ys = rowUnique ys
 
-allRowsGood ∷ Board → Bool
+allRowsGood ∷ Filter
 allRowsGood xss = all rowGood xss
 
-allColsGood ∷ Board → Bool
+allColsGood ∷ Filter
 allColsGood = allRowsGood ∘ transpose
 
-allSegmentsGood ∷ Board → Bool
+allSegmentsGood ∷ Filter
 allSegmentsGood [] = True
 allSegmentsGood xss = 
     let checkSegment zss = rowGood $ concat zss
@@ -63,3 +67,36 @@ allSegmentsGood xss =
             checkSplitCols (splitAt 3 $ transpose initss) ∧
             checkSplitRows (splitAt 3 tailss)
     in checkSplitRows ([], xss)
+
+lift ∷ [[Maybe Integer]] → Board
+lift = let
+    f Nothing = [1..9]
+    f (Just x) = [x]
+  in map (map f)
+
+board_set ∷ ([Integer] → [Integer] → [Integer]) → Board → Board → Board
+board_set f = zipWith (zipWith (\a b → a `f` b))
+
+board_intersect ∷ Board → Board → Board
+board_intersect = board_set intersect
+
+board_union ∷ Board → Board → Board
+board_union = board_set union
+
+possibilities ∷ Board → [Board]
+possibilities board = do
+  row_idx ← [1..length board]
+  let row = board !! row_idx
+  cell_idx ← [1.. length row]
+  let cell = row !! cell_idx
+  possibility ← cell
+  let newRow = (take (cell_idx-1) row) ++ [[possibility]] ++ (drop cell_idx row)
+  let newBoard = (take (row_idx-1) board) ++ [newRow] ++ (drop row_idx board)
+  return newBoard
+  
+
+basicApply ∷ Filter → Board → Board
+basicApply f b = foldl1 board_union $ filter f $ possibilities b
+
+--backtrackApply ∷ Filter → Board → Board
+--backtrackApply 
